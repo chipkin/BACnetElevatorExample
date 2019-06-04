329788de1178d0a85ff811f23699f629047c91a3
Steven Smethurst <ssmethurst@chipkin.com>
2019-06-04 16:58:24 -0700

    Linked the CAS BACnet stack Adapter and the CAS BACnet stack for a hello version program

---
+++ BACnetElevatorExample/BACnetElevatorExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using BACnetStackDLLServerCSharpExample;
+using System;
+
+namespace BACnetElevatorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BACnetServer bacnetServer = new BACnetServer();
+            bacnetServer.Run();
+        }
+
+        unsafe class BACnetServer
+        {
+            const string APPLICATION_VERSION = "0.0.1";
+            public void Run()
+            {
+                Console.WriteLine("Starting BACnetElevatorExample version {0}", APPLICATION_VERSION);
+                Console.WriteLine("FYI: BACnet Stack version: {0}.{1}.{2}.{3}",
+                    CASBACnetStackAdapter.GetAPIMajorVersion(),
+                    CASBACnetStackAdapter.GetAPIMinorVersion(),
+                    CASBACnetStackAdapter.GetAPIPatchVersion(),
+                    CASBACnetStackAdapter.GetAPIBuildVersion());
+            }
+        }
+    }
+}
